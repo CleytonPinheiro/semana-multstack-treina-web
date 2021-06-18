b4fd76a079785c5111136e74190ffb591b3a4820
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-06-18 18:00:54 -0300

    Aula1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import Head from "next/head";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 
 export default function Home() {
   return (
-    <div >
-      
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheças os profissionais"}
+        subtitle={
+          "Preencha com seu endereço e conheça os profissionais da sua área"
+        }
+      />
     </div>
-  )
+  );
 }
---
+++ src/ui/components/surfaces/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { HeaderAppBar, HeaderLogo } from "./Header.style";
+import { Toolbar } from "@material-ui/core";
+
+const Header: React.FC = () => {
+  return (
+    <HeaderAppBar position={"sticky"}>
+      <Toolbar>
+        <HeaderLogo
+          src={"/img/logos/logo.svg"}
+          alt={"e-diaristas"}
+        ></HeaderLogo>
+      </Toolbar>
+    </HeaderAppBar>
+  );
+};
+
+export default Header;
