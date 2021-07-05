d2096167ddbc6cb071118393835fc80567944763
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-07-04 23:37:04 -0300

    configurando axios

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -1,6 +1,7 @@
 import { useState, useMemo } from "react";
 import { UserShortInterface } from "data/@types/UserInterface";
 import { ValidationService } from "data/services/ValidationService";
+import { ApiService } from "data/services/ApiService";
 
 export default function useIndex() {
   const [cep, setCep] = useState(""),
@@ -12,6 +13,23 @@ export default function useIndex() {
     [carregando, setCarregando] = useState(false),
     [diaristas, setDiaristas] = useState([] as UserShortInterface[]),
     [diaristasRestantes, setDiaristasRestantes] = useState(0);
+
+  async function buscarProfissionais(cep: string) {
+    setBuscaFeita(false);
+    setCarregando(true);
+    setErro("");
+
+    try {
+      await ApiService.get(
+        "/api/diaristas-cidade?cep=" + cep.replace(/\D/g, "")
+      );
+      setBuscaFeita(true);
+      setCarregando(false);
+    } catch (error) {
+      setErro("CEP não encontrado");
+      setCarregando(false);
+    }
+  }
 
   return {
     cep,
