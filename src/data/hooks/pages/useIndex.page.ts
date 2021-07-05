f99a60be831bdbc19a212e8cd84a0274cbde7456
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-07-04 23:05:33 -0300

    Alternativa ao useState

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { UserShortInterface } from "data/@types/UserInterface";
+import { ValidationService } from "data/services/ValidationService";
 
 export default function useIndex() {
   const [cep, setCep] = useState(""),
     [erro, setErro] = useState(""),
+    cepValido = useMemo(() => {
+      return ValidationService.cep(cep);
+    }, [cep]),
     [buscaFeira, setBuscaFeita] = useState(false),
     [carregando, setCarregando] = useState(false),
-    [diaristas, setDiaristas] = useState([] as UserShortInterface[]);
+    [diaristas, setDiaristas] = useState([] as UserShortInterface[]),
+    [diaristasRestantes, setDiaristasRestantes] = useState(0);
 
   return {
     cep,
     setCep,
+    cepValido,
   };
 }
